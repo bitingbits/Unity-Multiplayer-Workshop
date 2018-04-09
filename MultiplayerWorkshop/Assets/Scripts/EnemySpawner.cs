200cec62e9f108bf515f9fbe35b1912eac470e65
Caprico1 <suprn8@live.com>
2018-04-08 19:44:27 -0500

    initial workshop build...

---
+++ MultiplayerWorkshop/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class EnemySpawner : NetworkBehaviour
+{
+
+    public GameObject enemyPrefab;
+    //public int numberOfEnemies;
+
+    public override void OnStartServer()
+    {
+        var enemy = (GameObject)Instantiate(enemyPrefab, this.transform.position, this.transform.rotation);
+        NetworkServer.Spawn(enemy);
+        //for (int i = 0; i < numberOfEnemies; i++)
+        //{
+        //    var spawnPosition = new Vector3(
+        //        Random.Range(-8.0f, 8.0f),
+        //        0.0f,
+        //        Random.Range(-8.0f, 8.0f));
+
+        //    var spawnRotation = Quaternion.Euler(
+        //        0.0f,
+        //        Random.Range(0, 180),
+        //        0.0f);
+
+        //    var enemy = (GameObject)Instantiate(enemyPrefab, spawnPosition, spawnRotation);
+        //    NetworkServer.Spawn(enemy);
+        //}
+    }
+}
